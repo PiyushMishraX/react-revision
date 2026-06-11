980855cc42365dbdb36556f663baf29d0107aa88
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-06-11 13:28:18 +0530

    memo for optimization

--- day 8/src/App.jsx
+++ day 8/src/App.jsx
@@ -1,55 +1,123 @@
-import React, { useMemo } from 'react'
-import useCounter from './useCounter'
-import './App.css'
+// import React, { useMemo } from 'react'
+// import useCounter from './useCounter'
+// import './App.css'
+
+// const App = () => {
+
+//   let { count, inc, dec, reset} = useCounter(0);
+
+//   // function call() {
+//   //   let res = 0;
+//   //   for (let i = 0 + 1; i <= 1000000000; i++) {
+//   //     res += i;
+//   //   }
+//   //   return res;
+//   // }
+
+
+//   // let total = useMemo(() => {
+//   //   return call();
+//   // }, [])
+  
+//   // let total = useMemo(() => {
+//   // function call() {
+//   //   let res = 0;
+//   //   for (let i = 0 + 1; i <= 1000000000; i++) {
+//   //     res += i;
+//   //   }
+//   //   return res;
+//   // }
+//   //   return call();
+//   // }, [])
+  
+//   let total = useMemo(() => {
+//     let res = 0;
+//     for (let i = 0 + 1; i <= 1000000000; i++) {
+//       res += i;
+//     }
+//     return res;
+//   }, [])
+
+  
+
+//   // let total = call();
+//   return (
+//     <div>
+//       <h1>{count}</h1>
+//       <h2>{total}</h2>
+//       <button className='btn' onClick={inc}>INc</button>
+//       <button className='btn' onClick={dec}>DEc</button>
+//       <button className='btn' onClick={reset}>Reset</button>
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+
+// import React, { useState } from 'react'
+
+// const App = () => {
+//   let [ count, setCount ] = useState(0)
+//   return (
+//     <div>
+//       <h2>{count}</h2>
+//       <button onClick={()=>setCount(count+1)}>add</button>
+//       <Child/>
+      
+//     </div>
+//   )
+// }
+
+// // <Child /> reloads too multiple times  which is unneccesary
+
+// const Child = () => {
+//   console.log("hello")
+
+//   return (
+//     <div>
+//       hello
+//     </div>
+//   )
+// }
+
+
+// export default App
+
+
+
+
+import React, { memo, useState } from 'react'
 
 const App = () => {
-
-  let { count, inc, dec, reset} = useCounter(0);
-
-  // function call() {
-  //   let res = 0;
-  //   for (let i = 0 + 1; i <= 1000000000; i++) {
-  //     res += i;
-  //   }
-  //   return res;
-  // }
-
-
-  // let total = useMemo(() => {
-  //   return call();
-  // }, [])
-  
-  // let total = useMemo(() => {
-  // function call() {
-  //   let res = 0;
-  //   for (let i = 0 + 1; i <= 1000000000; i++) {
-  //     res += i;
-  //   }
-  //   return res;
-  // }
-  //   return call();
-  // }, [])
-  
-  let total = useMemo(() => {
-    let res = 0;
-    for (let i = 0 + 1; i <= 1000000000; i++) {
-      res += i;
-    }
-    return res;
-  }, [])
-
-  
-
-  // let total = call();
+  let [ count, setCount ] = useState(0)
   return (
     <div>
-      <h1>{count}</h1>
-      <h2>{total}</h2>
-      <button className='btn' onClick={inc}>INc</button>
-      <button className='btn' onClick={dec}>DEc</button>
-      <button className='btn' onClick={reset}>Reset</button>
+      <h2>{count}</h2>
+      <button onClick={()=>setCount(count+1)}>add</button>
+      <Child/>
+      
     </div>
   )
 }
 
+// memo is a Higher-Order Component (HOC) used to wrap an entire component to prevent it from re-rendering.
+// useMemo is a React Hook used inside a component to cache the result of a heavy calculation or formula.
+
+let Child = memo(() => {
+  console.log("hello")
+
+  return (
+    <div>
+      hello
+    </div>
+  )
+})
+
+
+
 export default App
+
+
+
--- day 8/src/useCounter.jsx
+++ day 8/src/useCounter.jsx
@@ -1,25 +1,25 @@
-import { useState } from "react";
+// import { useState } from "react";
 
-function useCounter(inital=0) {
-    let [ count, setCount ]=  useState(inital)
-    const inc = async ()=> await setCount(count+1)
-    const dec = async ()=> await setCount(count-1)
-    const reset = ()=> setCount(0)
+// function useCounter(inital=0) {
+//     let [ count, setCount ]=  useState(inital)
+//     const inc = async ()=> await setCount(count+1)
+//     const dec = async ()=> await setCount(count-1)
+//     const reset = ()=> setCount(0)
 
 
-    return { count, inc, dec, reset}
+//     return { count, inc, dec, reset}
     
-}
-
-
-// import React from 'react'
-
-// const useCounter = () => {
-
-    
-//   return (
-//     <div>useCounter</div>
-//   )
 // }
 
-export default useCounter
+
+// // import React from 'react'
+
+// // const useCounter = () => {
+
+    
+// //   return (
+// //     <div>useCounter</div>
+// //   )
+// // }
+
+// export default useCounter
